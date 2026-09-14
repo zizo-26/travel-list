bd114fc62c763f9bcb2b846fc4794247db3210fc
zizo-26 <abdelazizbon24@gmail.com>
2026-09-14 12:13:22 +0200

    setting logo and form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 
 
+function App() {
+  return (
+    <div>
+      <Logo/>
+      <Form/>
+      
+      
+    </div>
+  );
+}
+
+
+function Logo(){
+
+  return(<h1>far away</h1>)
+}
+
+function Form(){
+
+
+  return(<div className="add-form">
+    <h3>what do you need for this trip</h3>
+  </div>)
+}
+
+
 
 
 
